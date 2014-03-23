06ba58a5333721471980f81667bc3ba3425057ff
Jeremy Laflin <laflijj@gmail.com>
2014-03-23 18:51:46 -0400

    added .py script for running remotely

--- dipole-rotation/dipole-rotation-v4.py
+++ dipole-rotation/dipole-rotation-v4.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
 # import modules
 import numpy as np
 
@@ -11,12 +16,16 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
+# <codecell>
+
 # Note: this transforms A to B
 def DCM(theta):
     C = np.array([[np.cos(theta), -np.sin(theta), 0],
                   [np.sin(theta),  np.cos(theta), 0],
                   [0, 0, 1]])
     return C
+
+# <codecell>
 
 # function that converts cartesian coordinates to pherical
 def cart_to_sph(r):
@@ -41,6 +50,8 @@ def cart_to_sph(r):
         r_sph = [dict(zip(['rho','alpha','beta'], r)) for r in r_sph]
         return r_sph
 
+# <codecell>
+
 # compute spherical harmonics using semi-normalized formula
 def my_sph(m,n,theta,phi):
     x = np.cos(theta)
@@ -48,6 +59,8 @@ def my_sph(m,n,theta,phi):
     Pmn = (-1)**np.abs(m)*(1-x**2)**(np.abs(m)/2)*sc.special.eval_legendre((n-np.abs(m)), x)
     Ymn = C*Pmn*sc.exp(1j*m*phi)
     return Ymn
+
+# <codecell>
 
 class ChargedBody:
     """
@@ -150,7 +163,15 @@ class ChargedBody:
         Phi = np.sum([np.sum([self.M[n][m+n]/rp['rho']**(n+1)
                               *my_sph(m, n, rp['alpha'], rp['beta']) 
                               for m in range(-n,n+1)]) for n in range(self.p+1)])
+#         [[print('M[',n,'][',m+n,']= ',"{0:.3f}".format(self.M[n][m+n]),
+#                 'rp^n = ',"{0:.3f}".format(rp['rho']**(n+1)),
+#                 'Y(theta,phi) = ',"{0:.3f}".format(my_sph(m, n, rp['alpha'], rp['beta']))) 
+#                               for m in range(-n,n+1)] for n in range(self.p+1)]
+#         [print('Phi[',n,'] = ', "{0:.3f}".format(np.sum([self.M[n][m+n]/rp['rho']**(n+1)*my_sph(m, n, rp['alpha'], rp['beta']) 
+#                               for m in range(-n,n+1)]))) for n in range(self.p+1)]
         return Phi
+
+# <codecell>
 
 def example(zeta,d,p):
     # Describe system        
@@ -158,11 +179,14 @@ def example(zeta,d,p):
     a = 1
 
     # charge values [O H H]
-    q = [1, -1]
+    q = [-1, 1]
 
     # location of charges w.r.t origin
     roq =  np.array([[-a/2, 0, 0],
                      [ a/2, 0, 0]])
+    
+    # Define test point
+    rcq_p = np.array([0, -d, 0])
 
 
     # Compute the center of charge and locate particles w.r.t. center of charge
@@ -183,9 +207,6 @@ def example(zeta,d,p):
     rcq_qbT = np.dot(CAB,rcq_qa.T)
     rcq_qb = rcq_qbT.T
 
-    # Define test point
-    rcq_p = np.array([0, -d, 0])
-
     # Transform coordinates of point of interest
     rcq_p_sph = cart_to_sph(np.array([rcq_p]))
 
@@ -202,14 +223,15 @@ def example(zeta,d,p):
     bodyB.mul_exp(p)
 
     # Evaluate Potential at a point [r theta phi]
-    PhiA = bodyA.potential(rcq_p_sph)
+#     PhiA = bodyA.potential(rcq_p_sph)
     PhiB = bodyB.potential(rcq_p_sph)
-    # print("Potential (via M.E.) of System B at point 'p' = ",PhiB.real)
+    PhiB = PhiB.real
+#     print("Potential (via M.E.) of System B at point 'p' = ",PhiB.real)
 
     # Compute the exact solution 
     VB = np.sum([qb/np.linalg.norm(-rcq_p + r) for qb,r in zip(q,rcq_qb)])
-    # print("Potential (exact) of System A at point 'p' = ",VA)
-    # print("Potential (exact) of System B at point 'p' = ",VB)
+#     print("Potential (exact) of System A at point 'p' = ",VA)
+#     print("Potential (exact) of System B at point 'p' = ",VB)
 
     # Perform a rotation on System A so that it is the same configuration as B 
     alpha = -zeta
@@ -219,49 +241,149 @@ def example(zeta,d,p):
 
     # Evaluate potential of 'A' at 'B'
     PhiA_B  = bodyA.potential(rcq_p_sph)
-
-    # Compute error and print results
-    error_exact = np.abs((PhiA_B-VB)/VB)*100
-    error_rot = np.abs((PhiA_B-PhiB)/PhiB)*100
-
-    rr = np.linalg.norm(rcq_p)
-    aa = np.max([r['rho'] for r in rcq_qb_sph])
-
+    PHiA_B = PhiA_B.real
+    
     # Compute the error bound
-    bound = (np.sum(np.abs(q))/(rr-aa)*(aa/rr)**(p+1))/VB*100
+    rr = np.linalg.norm(rcq_p)
+    #     [print('rho = ',r['rho']) for r in rcq_qb_sph]
+    aa = np.max([rq['rho'] for rq in rcq_qb_sph])
+    #     print('a = ',a)
+    Q = np.sum(np.abs(q))
+    #     print('Q = ',Q)
+    #     print('r - a = ',r - a)
+    #     print('a/r = ',a/r)
+    #     bound = Q/(rr-aa)*(aa/rr)**(p+1)/np.abs(VB)*100
+    bound = Q/(rr-aa)*(aa/rr)**(p+1)
+    return VB, PhiB, PhiA_B, bound
 
-    # print("Potential (via M.E.) of System A rot to B evaluated at point 'p' = ",PhiA_B.real)
-    # print("% Error compared to exact = ","{0:.3f}".format(error_exact.real),'%')
-    # print("% Error bound = ","{0:.3f}".format(bound),'%')
-    # print("% Error compared to potential using M.E. of 'B' = ","{0:.3f}".format(error_rot.real),'%')
-    return error_exact.real
+# <markdowncell>
 
-# run example over zeta = 2*pi and d = 1-10 
-theta = np.linspace(10.0*np.pi/180,350.0*np.pi/180,100)
-R = np.linspace(1.0,3.0,100)
-error = np.array([[example(zeta,d,3) \
-                   for zeta in theta] for d in R])
+# ### Try theta = 5 to 355 degrees and R/L = 1 to 10
+
+# <codecell>
+
+p  = 3
+# Terms in multipole exansion 
+
+# Number of data points in range
+n_points = 20
+
+# Create variable arrays
+theta = np.linspace(0,2*np.pi,n_points)
+dist = np.linspace(.6,2.5,n_points)
+
+# Evaluate Error in List Comprehension
+results = np.array([[example(zeta,d,p) \
+                     for d in dist] for zeta in theta])
+#                      for zeta in theta] for d in dist]);
+
+VB = results[:,:,0].real
+PhiB = results[:,:,1].real
+PhiA_B = results[:,:,2].real
+bound = results[:,:,3].real
+VB_rms = np.sqrt(1/VB.size*np.linalg.norm(VB)**2)
+print(VB_rms)
+
+# Compute error and print results
+# error_exact = np.abs((VB - PhiA_B))
+error_exact = np.abs((VB - PhiA_B))/VB_rms*100
+error_rot = np.abs((PhiB - PhiA_B))/VB_rms*100
+bound = bound/VB_rms*100
+
+# print("Potential (via M.E.) of System A rot to B evaluated at point 'p' = ",PhiA_B.real)
+# print("% Error compared to exact = ","{0:.3f}".format(error_exact.real),'%')
+# print("% Error bound = ","{0:.3f}".format(bound),'%')
+# print("% Error compared to potential using M.E. of 'B' = ","{0:.3f}".format(error_rot.real),'%')
+
+# <codecell>
 
 # Plot the results
-matplotlib.rcParams['xtick.direction'] = 'out'
-matplotlib.rcParams['ytick.direction'] = 'out'
+%matplotlib inline
 
-# Create a simple contour plot with labels using default colors.  The
-# inline argument to clabel will control whether the labels are draw
-# over the line segments of the contour, removing the lines beneath
-# the label
-plt.figure()
+plt.close('all')
+
+R = (dist - 0.5)/1.0
+
+levels = np.linspace(0,20,10)
+
+plt.close('all')
+plt.figure(figsize=(10, 8))
+
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
-CS = plt.contour(R/0.5, theta*180/np.pi, error)
-plt.clabel(CS, inline=1, fontsize=10)
+# Plot the error vs. exact
+fig = plt.figure(figsize=(10, 8))
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+levels = np.linspace(0,20,10)
+CS = plt.contourf(R, theta*180/np.pi, error_exact, levels)
+CB = plt.colorbar(CS, extend='both')
+plt.title('Error vs Exact')
+plt.xlabel(r'$\frac{R}{L}$')
+plt.ylabel(r'Rotation Angle $\left(\theta\right)$')
+plt.savefig('ErrorExact.pdf')
+# plt.show()
 
-# make a colorbar for the contour lines
-CB = plt.colorbar(CS, shrink=0.8, extend='both')
+# Plot the error vs. multipole
+fig = plt.figure(figsize=(10, 8))
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+levels = np.linspace(0,20,10)
+CS = plt.contourf(R, theta*180/np.pi, error_rot, levels)
+CB = plt.colorbar(CS, extend='both')
+plt.title(r'Error vs Multipole')
+plt.xlabel(r'$\frac{R}{L}$')
+plt.ylabel(r'Rotation Angle $\left(\theta\right)$')
+plt.savefig('ErrorMultipole.pdf')
+# plt.show()
 
-plt.xlabel(r'$\displaystyle\frac{R}{L}$')
-plt.ylabel(r'$\displaystyle\theta$ (degrees)',fontsize=16)
-plt.title(r'Percent Error Compared to Exact')
 
-plt.show()
+# plt.subplot(2, 1, 1)
+# plt.contourf(R, theta*180/np.pi, error_exact, levels)
+# plt.title(r'Percent Error v.s. Exact')
+# plt.ylabel(r'Rotation Angle $\left(\theta\right)')
+
+# plt.subplot(2, 1, 2)
+# plt.contourf(R, theta*180/np.pi, error_rot, levels)
+# plt.title(r'Percent Error v.s. Direct Spatial Multipole')
+# plt.xlabel(r'$\frac{R}{L}$')
+# plt.ylabel(r'Rotation Angle $\left(\theta\right)')
+
+# plt.savefig('ErrorSubplot.pdf')
+# plt.show()
+
+# # Two subplots, the axes array is 1-d
+# fig, axarr = plt.subplots(2, sharex=True)
+# axarr[0].contourf(R, theta*180/np.pi, error_exact, levels)
+# axarr[0].set_title(r'Percent Error v.s. Exact')
+# axarr[1].contourf(R, theta*180/np.pi, error_rot, levels)
+# axarr[1].set_title(r'Percent Error v.s. Direct')
+
+# # Plot the error bound
+# plt.figure()
+# levels = np.linspace(0,20,10)
+# CS = plt.contourf(R, theta*180/np.pi, bound, levels)
+# # plt.clabel(CS, inline=1, fontsize=10)
+# # make a colorbar for the contour lines
+# CB = plt.colorbar(CS, extend='both')
+# plt.clabel(CS, fontsize=8, inline=1)
+# plt.title('Error Bound')
+# plt.savefig('ErrorBound', dpi=900)
+# plt.show()
+
+# <codecell>
+
+ p = 2
+# dist = 2
+# theta = 0*np.pi/180
+# error = example(theta,dist,p)
+# RL = (dist - 0.5)/0.5
+# print('R/L = ',RL,'theta = ',theta*180/np.pi)
+# print('% Error Compared to Exact = ',error[0])
+# print('% Error Compared to Direct Multipole = ',error[1])
+# print('% Error Bound= ',error[2])
+
+# <codecell>
+
+
